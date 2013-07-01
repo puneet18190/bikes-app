44cdb21fd3c88317e34a7ecf372d0eef44733211
steve <steve@bov.nu>
2013-07-01 06:33:41 +0100

    deployremovesendmail

--- app/helpers/domain_helper.rb
+++ app/helpers/domain_helper.rb
@@ -4,16 +4,16 @@ module DomainHelper
   def domain_suff
     domain_suff = request.host
       logger.info "Domain #{domain_suff}"
-    if domain_suff.include? 'bike' then domain_suff = ''  end
-    if domain_suff.include? 'honda' then domain_suff = 'Honda'  end
-    if domain_suff.include? 'honda' then domain_suff = 'Honda'  end
-    if domain_suff.include? 'honda' then domain_suff = 'Honda'  end
-    if domain_suff.include? 'honda' then domain_suff = 'Honda'  end
-    if domain_suff.include? 'honda' then domain_suff = 'Honda'  end
-    if domain_suff.include? 'honda' then domain_suff = 'Honda'  end
-    if domain_suff.include? 'honda' then domain_suff = 'Honda'  end
+    if domain_suff.include? 'bike' then domain_suff = '' end
+    if domain_suff.include? 'honda' then domain_suff = 'Honda'     end
+  #  if domain_suff.include? 'honda' then domain_suff = 'Honda'
+  #  if domain_suff.include? 'honda' then domain_suff = 'Honda'
+  #  if domain_suff.include? 'honda' then domain_suff = 'Honda'
+  #  if domain_suff.include? 'honda' then domain_suff = 'Honda'
+  #  if domain_suff.include? 'honda' then domain_suff = 'Honda'
+  #  if domain_suff.include? 'honda' then domain_suff = 'Honda'
 
-    if domain_suff.include? 'honda' then logger.info "Honda"  end
+    if domain_suff.include? 'honda' then logger.info "Honda"     end
     logger.info "Make #{domain_suff}"
     @rv = domain_suff
   end
@@ -22,18 +22,18 @@ module DomainHelper
     domain_suff = request.host
     logger.info "Image Make #{domain_suff}"
 
-    if domain_suff.include? 'honda' then cl_image_tag("/assets/honda_cbr_600rr_black_wide.jpg", :width => 4000, :height => 5500, :crop => :fill)  end
-    if domain_suff.include? 'bike' then cl_image_tag("/assets/VT750.png", :width => 4000, :height => 5500, :crop => :fill)  end
-#    if domain_suff.include? 'honda' then cl_image_tag("/assets/honda_cbr_600rr_black_wide.jpg", :width => 4000, :height => 5500, :crop => :fill)  end
-#    if domain_suff.include? 'honda' then cl_image_tag("/assets/honda_cbr_600rr_black_wide.jpg", :width => 4000, :height => 5500, :crop => :fill)  end
-#    if domain_suff.include? 'honda' then cl_image_tag("/assets/honda_cbr_600rr_black_wide.jpg", :width => 4000, :height => 5500, :crop => :fill)  end
-#    if domain_suff.include? 'honda' then cl_image_tag("/assets/honda_cbr_600rr_black_wide.jpg", :width => 4000, :height => 5500, :crop => :fill)  end
-#    if domain_suff.include? 'honda' then cl_image_tag("/assets/honda_cbr_600rr_black_wide.jpg", :width => 4000, :height => 5500, :crop => :fill)  end
-#    if domain_suff.include? 'honda' then cl_image_tag("/assets/honda_cbr_600rr_black_wide.jpg", :width => 4000, :height => 5500, :crop => :fill)  end
-#    if domain_suff.include? 'honda' then cl_image_tag("/assets/honda_cbr_600rr_black_wide.jpg", :width => 4000, :height => 5500, :crop => :fill)  end
-    #if domain_suff.include? 'bike' then logger.info "Serve generic bike image"  end
-    #if domain_suff.include? 'honda' then logger.info "Serve Honda bike image"  end
+   cl_image_tag("/assets/honda_cbr_600rr_black_wide.jpg", :width => 4000, :height => 5500, :crop => :fill) if domain_suff.include? 'honda'
+   cl_image_tag("/assets/VT750.png", :width => 4000, :height => 5500, :crop => :fill) if domain_suff.include? 'bike'
+#    if domain_suff.include? 'honda' then cl_image_tag("/assets/honda_cbr_600rr_black_wide.jpg", :width => 4000, :height => 5500, :crop => :fill)
 
+ #    if domain_suff.include? 'honda' then cl_image_tag("/assets/honda_cbr_600rr_black_wide.jpg", :width => 4000, :height => 5500, :crop => :fill)
+#    if domain_suff.include? 'honda' then cl_image_tag("/assets/honda_cbr_600rr_black_wide.jpg", :width => 4000, :height => 5500, :crop => :fill)
+#    if domain_suff.include? 'honda' then cl_image_tag("/assets/honda_cbr_600rr_black_wide.jpg", :width => 4000, :height => 5500, :crop => :fill)
+#    if domain_suff.include? 'honda' then cl_image_tag("/assets/honda_cbr_600rr_black_wide.jpg", :width => 4000, :height => 5500, :crop => :fill)
+#    if domain_suff.include? 'honda' then cl_image_tag("/assets/honda_cbr_600rr_black_wide.jpg", :width => 4000, :height => 5500, :crop => :fill)
+#    if domain_suff.include? 'honda' then cl_image_tag("/assets/honda_cbr_600rr_black_wide.jpg", :width => 4000, :height => 5500, :crop => :fill)
+ #   if domain_suff.include? 'bike' then logger.info "Serve generic bike image"
+ #   if domain_suff.include? 'honda' then logger.info "Serve Honda bike image"
 
   end
 
--- config/application.rb
+++ config/application.rb
@@ -12,7 +12,7 @@ end
 module Backend
   class Application < Rails::Application
 
-    ActionMailer::Base.delivery_method = :sendmail
+   # ActionMailer::Base.delivery_method = :sendmail
     ActionMailer::Base.perform_deliveries = true
     ActionMailer::Base.raise_delivery_errors = true
 
