7bfc1ba65febe71c367b15b2dc59e23c7c2a6169
steve <steve@bov.nu>
2013-07-13 23:45:59 +0100

    rails4A

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,9 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Backend::Application.config.secret_token = 'a5f71929f2641dccc10b3ce6103dbb40b0aef294d9d307398c16f872729904f9583ab9c0c91dbea44ee2cb332e1ae0cbd5e5afb92e46f16a59bbc5298f574129'
+#Backend::Application.config.secret_token = 'a5f71929f2641dccc10b3ce6103dbb40b0aef294d9d307398c16f872729904f9583ab9c0c91dbea44ee2cb332e1ae0cbd5e5afb92e46f16a59bbc5298f574129'
+
+
+Backend::Application.config.secret_token = 'sb762a2f23950e306261908d4e5519ffe71ce626b119e9fc03a012ba86f46d82ef32d72f283633bacc2f59cf94ce5968552fe97d157e7f00560c1217d4592dda09'
+Backend::Application.config.secret_key_base = 'sbxx762a2f23950e306261908d4e5519ffe71ce626b119e9fc03a012ba86f46d82ef32d72f283633bacc2f59cf94ce5968552fe97d157e7f00560c1217d4592dda09'
+
